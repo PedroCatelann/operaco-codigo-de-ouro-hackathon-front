612154b1cb6a0f5e1c46cdb3e308397b7bf78cbd
Pedro Catelan <pedrohenrique.catelan@autbank.com.br>
2025-08-10 18:34:48 -0300

    comentando variável

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-const NGROK_HOST = "c1c79a0382f6.ngrok-free.app";
+//const NGROK_HOST = "c1c79a0382f6.ngrok-free.app";
 
 // https://vite.dev/config/
 export default defineConfig({
