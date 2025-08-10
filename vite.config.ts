0e23df7eb0354cee6afb144ebacb924b2d9ff5e4
Pedro Catelan <pedrohenrique.catelan@autbank.com.br>
2025-08-10 18:33:54 -0300

    ajuste build

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,11 @@ const NGROK_HOST = "c1c79a0382f6.ngrok-free.app";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
+  /* server: {
     host: "0.0.0.0", // permite acesso externo
     allowedHosts: [NGROK_HOST], // adiciona a URL do ngrok como host permitido
+  }, */
+  build: {
+    outDir: "dist",
   },
 });
